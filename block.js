ca502a905ffd81bb1a3a7586e96c941d75d2eaab
coderNishitha <70238943+coderNishitha@users.noreply.github.com>
2020-11-15 12:38:07 -0800

    Add files via upload

---
+++ block.js
@@ -0,0 +1,43 @@
+class Box{
+    constructor(x,y,width,height) {
+      var options = {
+        restitution:0.8,
+        friction:1.0,
+        density:1.0
+          
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      this.visibility = 255;
+      World.add(world, this.body);
+    }
+
+    display(){
+
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      
+      if(this.body.speed < 4){
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("rgb(46, 185, 240)");
+        rect(0, 0, this.width, this.height);
+        pop();
+       }
+       else{
+         World.remove(world, this.body);
+         push();
+         this.Visiblity = this.Visiblity - 5;
+         tint(255,this.Visiblity);
+         pop();
+       }
+      }  
+      score(){
+        if(this.visiblity<0 && this.visiblity >-105){
+          score++
+        }
+        }
+  }
